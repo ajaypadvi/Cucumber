51cd2c30f52edfb0dc9c90deb8751548465ca342
Ajay Padvi <ajaypadvi@gmail.com>
2018-11-24 19:09:48 +0530

    Removed the .idea folder

---
+++ src/test/java/cucumberOptions/TestRunner.java
@@ -0,0 +1,10 @@
+package cucumber.options;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/java/features",glue = "definitions")
+public class TestRunner {
+}
