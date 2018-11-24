ce9e03ea27f41f6cd4073fd7c873a7fcc36606e2
Ajay Padvi <ajaypadvi@gmail.com>
2018-11-24 14:54:35 +0530

    refactored code packages for definitions

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Cucumber
+
+
+
+Notes:
+1.To generate step definitions files automatically>>
+If you want to generate the step diminutions for the gherkin syntax written in feature file then do alt+enter and you will get a prompt to specify the java file in which
+the step definitions would be created.
+This is all done using Cucumber tool plugin in intellij
+
---
+++ src/test/java/definitions/ApplicationLoginStepdefs.java
@@ -0,0 +1,33 @@
+package definitions;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class ApplicationLoginStepdefs {
+    @Given("^User is on net banking login page$")
+    public void userIsOnNetBankingLoginPage() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^User login to application with username and password$")
+    public void userLoginToApplicationWithUsernameAndPassword() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^Home page is populated$")
+    public void homePageIsPopulated() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^Cards are displayed$")
+    public void cardsAreDisplayed() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
