f7ba6daceede02f42206059ff0156e2f63a4da0f
Ajay Padvi <ajaypadvi@gmail.com>
2018-11-24 20:18:17 +0530

    Datatables for multiple parameters and parameterization using Scenario Outline and Example keywords

--- src/test/java/definitions/ApplicationLoginStepdefs.java
+++ src/test/java/definitions/ApplicationLoginStepdefs.java
@@ -1,10 +1,13 @@
 package definitions;
 
+import cucumber.api.DataTable;
 import cucumber.api.PendingException;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+
+import java.util.List;
 
 public class ApplicationLoginStepdefs {
     @Given("^User is on net banking login page$")
@@ -33,6 +36,7 @@ public class ApplicationLoginStepdefs {
         // Write code here that turns the phrase above into concrete actions
         // throw new PendingException();
         System.out.println("Validated the cards on the home page");
+        System.out.println("--");
     }
 
     @When("^User login to application with \"([^\"]*)\" and \"(\\d+)$")
@@ -55,5 +59,35 @@ public class ApplicationLoginStepdefs {
         // Write code here that turns the phrase above into concrete actions
         // throw new PendingException();
         System.out.println("Validated that the cards are not displayed on the home page");
+        System.out.println("--");
+    }
+
+    @When("^User signs up to application with following details$")
+    public void userSignsUpToApplicationWithFollowingDetails(DataTable rows) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        // throw new PendingException();
+        List<List<String>> object = rows.raw();
+        List<String> record = object.get(0);
+        System.out.println("User signing up into application with following details");
+        System.out.println("Name: " + record.get(0));
+        System.out.println("Password: " + record.get(1));
+        System.out.println("Email: " + record.get(2));
+        System.out.println("City: " + record.get(3));
+        System.out.println("Phone Number: " + record.get(4));
+    }
+
+    @And("^Cards displayed are \"([^\"]*)\"$")
+    public void cardsDisplayedAre(String displayCards) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        // throw new PendingException();
+        System.out.println("Cards to be displayed: " + displayCards);
+        System.out.println("--");
+    }
+
+    @When("^User logs into application with (.+) and (.+)$")
+    public void userLoginToApplicationWithUserNameAndPassword(String username, String password) throws Throwable {
+        System.out.println("UserName: " + username + ", Password" + password);
+        // Write code here that turns the phrase above into concrete actions
+        // throw new PendingException();
     }
 }
